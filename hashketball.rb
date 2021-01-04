d3bae8b22f654ae0643204c550e6eda516066061
Abraham Aviles <aaaviles1995@gmail.com>
2021-01-04 00:00:19 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -145,19 +145,47 @@ end
   returns the number of rebounds of the player with the biggest shoe size (FAILED - 8)
 =end
 
+def num_points_scored
+  
+  
+  
+end
 
+def shoe_size
+  
+  
+  
+end
 
+def team_colors
+  
+  
+  
+end
 
+def team_names
+  
+  
+  
+end
 
+def player_numbers
+  
+  
+  
+end
 
+def player_stats
+  
+  
+  
+end
 
-
-
-
-
-
-
-
+def big_shoe_rebounds
+  
+  
+  
+end
 
 
 
